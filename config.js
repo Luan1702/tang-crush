3fd8f0bcad617d7063db5a40e47a79bdd9902586
Luan1702 <83948243+Luan1702@users.noreply.github.com>
2021-06-30 16:09:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    introTitle: 'Triều ngu',
+    introDesc: `Yêu em nhiều lòng này nhói đau :((`,
+    btnIntro: 'Bấm vô thằng ngu',
+    title: 'Mi có yêu bé Ngọc Trương không Triều',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'Địt mẹ thích vãi lồn <33',
+    btnNo: 'Không, Đéo thích :3',
+    question:'Trả lời thật lòng đi bạn',
+    btnReply: 'Nhập vô đi anh zai <3',
+    reply: 'Ây Da Nhớ bé Ngọc vãi lồn, chừ sao đây <33333333',
+    mess: 'Ta biết ngay mà  🥰. Lồn ngu hihi 😘😘',
+    messDesc: 'Tối Đi đạp xe không bạn.',
+    btnAccept: 'Okiiiii  ik luôn  <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
